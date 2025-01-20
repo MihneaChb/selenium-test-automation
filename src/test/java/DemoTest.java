bc360a08dd3bbfc6b9ee8e96a41211c87e06a2a1
kelbo18 <zmihchbz@gmail.com>
2025-01-20 18:12:50 +0200

    -added logs library, allure report

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,14 +1,23 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.automation.framework.BrowserManager;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class DemoTest {
+
     BrowserManager manager = new BrowserManager();
+    Logger log = LogManager.getRootLogger();
+
     @Test
     public void browserTest() {
         manager.openBrowser();
+        log.info("Browser opened");
         WebDriver driver = manager.getDriver();
-        driver.get("https://www.google.com");
+        log.warn("This is a warning message");
+        driver.get("https://www.youtube.com");
         String url = driver.getCurrentUrl();
         assertTrue(url.contains("google"));
     }
